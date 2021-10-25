e4594ed2bc6b66e3c47d2f86a4149b605eec46e7
Alejandro Toro Minguez <atoro@deloitte.es>
2021-10-25 17:05:06 +0200

    primer commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,20 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { heroesModule } from './heroes/heroe/heroes.module';
+import { contadorModule } from './contador/contador/contador.module';
+
+// Crear modulo llamado ContadorModule
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    heroesModule, // importar aqui
+    contadorModule
   ],
   providers: [],
   bootstrap: [AppComponent]
